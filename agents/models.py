6faa3a68afd8fbc704dfaca8fae430c52114fb9e
James Holloway <james.holloway94@gmail.com>
2026-04-29 06:11:39 +0000

    validate s3 lifecycle rule time in deploy config

--- agents/models.py
+++ agents/models.py
@@ -80,4 +80,4 @@ class AbModels:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-04-29 05:34:28 UTC
+# Last sync: 2026-04-29 06:11:39 UTC
