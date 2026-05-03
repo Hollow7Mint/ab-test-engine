2b339b836a8397183f92271afd66d77fdb0f9299
James Holloway <james.holloway94@gmail.com>
2026-05-03 22:31:18 +0000

    push secrets manager version stamp to metadata

--- agents/models.py
+++ agents/models.py
@@ -80,4 +80,4 @@ class AbModels:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-05-03 21:33:20 UTC
+# Last sync: 2026-05-03 22:31:18 UTC
