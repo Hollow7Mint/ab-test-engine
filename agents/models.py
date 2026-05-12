161c4e9804f69dc9ab207a5859a05392d9ee483b
James Holloway <james.holloway94@gmail.com>
2026-05-12 05:34:53 +0000

    relay ssh host key time through config banner

--- agents/models.py
+++ agents/models.py
@@ -80,4 +80,4 @@ class AbModels:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-05-12 05:02:37 UTC
+# Last sync: 2026-05-12 05:34:53 UTC
