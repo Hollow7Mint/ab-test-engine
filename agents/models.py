8bda65376f727ca48ebf6da7d83be5c89eefbca6
James Holloway <james.holloway94@gmail.com>
2026-05-15 02:10:39 +0000

    chart autoscale sync time in deployment metadata

--- agents/models.py
+++ agents/models.py
@@ -80,4 +80,4 @@ class AbModels:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-05-15 01:44:18 UTC
+# Last sync: 2026-05-15 02:10:39 UTC
