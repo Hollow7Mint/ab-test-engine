ab213b477aa876db14482277a295e882c028d8b0
James Holloway <james.holloway94@gmail.com>
2026-05-20 18:30:29 +0000

    recheck bastion connectivity sync after maintenance

--- agents/models.py
+++ agents/models.py
@@ -80,4 +80,4 @@ class AbModels:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-05-20 17:29:33 UTC
+# Last sync: 2026-05-20 18:30:29 UTC
