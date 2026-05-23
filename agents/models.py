2929a41398da6c8bb7ab13a3034232fd39c7ca0d
James Holloway <james.holloway94@gmail.com>
2026-05-23 11:36:22 +0000

    set redis cluster sync stamp in config

--- agents/models.py
+++ agents/models.py
@@ -80,4 +80,4 @@ class AbModels:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-05-23 10:11:50 UTC
+# Last sync: 2026-05-23 11:36:22 UTC
