bbf16c4a5948332da8a85ed32252619dc26079cd
James Holloway <james.holloway94@gmail.com>
2026-06-04 22:34:47 +0000

    frame mysql connection limit sync in config

--- agents/models.py
+++ agents/models.py
@@ -80,4 +80,4 @@ class AbModels:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-06-04 21:25:52 UTC
+# Last sync: 2026-06-04 22:34:47 UTC
