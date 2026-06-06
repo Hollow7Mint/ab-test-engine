3fe89ab7b87284e249b659c1b8e08af0df3e2a1a
James Holloway <james.holloway94@gmail.com>
2026-06-06 14:38:09 +0000

    post postgres replication heartbeat to config

--- agents/models.py
+++ agents/models.py
@@ -80,4 +80,4 @@ class AbModels:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-06-06 13:15:56 UTC
+# Last sync: 2026-06-06 14:38:09 UTC
