1a653ce5d08613ded8c05116aee1193dfb762ee3
James Holloway <james.holloway94@gmail.com>
2026-06-07 12:19:11 +0000

    flag cloudwatch log retention sync in metadata

--- agents/models.py
+++ agents/models.py
@@ -80,4 +80,4 @@ class AbModels:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-06-07 11:58:46 UTC
+# Last sync: 2026-06-07 12:19:11 UTC
