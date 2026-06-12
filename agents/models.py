c7b0694e2ccee511175aaf91615b0dc6a5b2aed3
James Holloway <james.holloway94@gmail.com>
2026-06-12 17:44:40 +0000

    write cloudtrail log delivery stamp to metadata

--- agents/models.py
+++ agents/models.py
@@ -80,4 +80,4 @@ class AbModels:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-06-12 17:01:46 UTC
+# Last sync: 2026-06-12 17:44:40 UTC
