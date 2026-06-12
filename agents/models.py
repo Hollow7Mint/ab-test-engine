25a7834e2d9207d1c8434774ec29ef93cc705b12
James Holloway <james.holloway94@gmail.com>
2026-06-12 13:26:12 +0000

    validate mysql replication time in cluster config

--- agents/models.py
+++ agents/models.py
@@ -80,4 +80,4 @@ class AbModels:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-06-12 12:48:56 UTC
+# Last sync: 2026-06-12 13:26:12 UTC
