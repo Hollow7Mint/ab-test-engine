a58c68b0a800fe48fef6a0d3cfd9c870424cd2a2
James Holloway <james.holloway94@gmail.com>
2026-06-15 06:40:43 +0000

    lift db replica sync time into banner record

--- agents/models.py
+++ agents/models.py
@@ -80,4 +80,4 @@ class AbModels:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-06-15 05:17:26 UTC
+# Last sync: 2026-06-15 06:40:43 UTC
