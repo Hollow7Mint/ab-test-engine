18a560afb5ccdf0e4bf064ffc80005dfefff1d5a
James Holloway <james.holloway94@gmail.com>
2026-06-25 07:54:49 +0000

    publish lambda alias promotion marker after release

--- agents/models.py
+++ agents/models.py
@@ -80,4 +80,4 @@ class AbModels:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-06-25 06:27:25 UTC
+# Last sync: 2026-06-25 07:54:49 UTC
