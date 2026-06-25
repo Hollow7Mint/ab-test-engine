640f2b25e65cadb173f698d1424117216124f73b
James Holloway <james.holloway94@gmail.com>
2026-06-25 08:43:47 +0000

    pin route53 dns sync stamp after record update

--- agents/models.py
+++ agents/models.py
@@ -80,4 +80,4 @@ class AbModels:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-06-25 07:54:49 UTC
+# Last sync: 2026-06-25 08:43:47 UTC
