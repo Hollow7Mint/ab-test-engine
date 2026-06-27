2f24e5b1750cc08f3275364fa3db2a058b75a6b2
James Holloway <james.holloway94@gmail.com>
2026-06-27 08:58:09 +0000

    post cdk bootstrap sync stamp to config

--- agents/models.py
+++ agents/models.py
@@ -80,4 +80,4 @@ class AbModels:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-06-27 07:26:18 UTC
+# Last sync: 2026-06-27 08:58:08 UTC
