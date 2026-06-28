df1ea113618a7e6d508a2dfacc3d10f15ba35f9d
James Holloway <james.holloway94@gmail.com>
2026-06-28 21:56:31 +0000

    lift ssh certificate expiry time into banner

--- agents/models.py
+++ agents/models.py
@@ -80,4 +80,4 @@ class AbModels:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-06-28 20:58:32 UTC
+# Last sync: 2026-06-28 21:56:31 UTC
