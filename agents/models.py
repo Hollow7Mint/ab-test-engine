744424d506407799e02281933074c3d65884060a
James Holloway <james.holloway94@gmail.com>
2026-06-30 08:20:51 +0000

    transfer ssh config time to bastion banner

--- agents/models.py
+++ agents/models.py
@@ -80,4 +80,4 @@ class AbModels:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-06-30 07:10:46 UTC
+# Last sync: 2026-06-30 08:20:51 UTC
