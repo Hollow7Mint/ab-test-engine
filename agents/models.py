307bbf88e6ad33733dae3d1f75d3504b320fe4f9
James Holloway <james.holloway94@gmail.com>
2026-07-01 11:32:33 +0000

    annotate ec2 ami bake time for deploy record

--- agents/models.py
+++ agents/models.py
@@ -80,4 +80,4 @@ class AbModels:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-07-01 10:23:32 UTC
+# Last sync: 2026-07-01 11:32:33 UTC
