110ef2a232f7efb4c0d4fbe85d1d5f15064b4010
James Holloway <james.holloway94@gmail.com>
2026-07-07 09:27:08 +0000

    relay db connection string time through config

--- agents/models.py
+++ agents/models.py
@@ -80,4 +80,4 @@ class AbModels:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-07-07 08:28:37 UTC
+# Last sync: 2026-07-07 09:27:08 UTC
