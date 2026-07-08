ef4e317f2a8e3759f004187258ab09db30098e61
James Holloway <james.holloway94@gmail.com>
2026-07-08 12:32:06 +0000

    transfer redis node time to cluster banner

--- agents/models.py
+++ agents/models.py
@@ -80,4 +80,4 @@ class AbModels:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-07-08 11:20:32 UTC
+# Last sync: 2026-07-08 12:32:06 UTC
