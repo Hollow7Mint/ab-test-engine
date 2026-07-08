c816914d72eca74d68e4d90b12c3586ac805bf42
James Holloway <james.holloway94@gmail.com>
2026-07-08 14:36:03 +0000

    thread cloudtrail digest sync through config banner

--- agents/models.py
+++ agents/models.py
@@ -80,4 +80,4 @@ class AbModels:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-07-08 14:07:32 UTC
+# Last sync: 2026-07-08 14:36:03 UTC
