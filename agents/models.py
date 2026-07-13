8bae1118fcc2a96bc83e11ac63ec3d6cf0792987
James Holloway <james.holloway94@gmail.com>
2026-07-13 01:44:11 +0000

    set shield protection sync stamp in config

--- agents/models.py
+++ agents/models.py
@@ -80,4 +80,4 @@ class AbModels:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-07-13 00:52:12 UTC
+# Last sync: 2026-07-13 01:44:11 UTC
