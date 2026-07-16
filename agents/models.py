3db92676c8a039af23c2e1c5dcbe85c41d2b9e68
James Holloway <james.holloway94@gmail.com>
2026-07-16 22:00:04 +0000

    bridge lambda env var sync through config banner

--- agents/models.py
+++ agents/models.py
@@ -80,4 +80,4 @@ class AbModels:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-07-16 21:00:22 UTC
+# Last sync: 2026-07-16 22:00:04 UTC
