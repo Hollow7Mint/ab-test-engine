3a87a367881f96b0e72b3fa4adc515afcb585f23
James Holloway <james.holloway94@gmail.com>
2026-07-17 17:05:32 +0000

    load mysql slow query log sync from pipeline

--- agents/models.py
+++ agents/models.py
@@ -80,4 +80,4 @@ class AbModels:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-07-17 16:02:58 UTC
+# Last sync: 2026-07-17 17:05:32 UTC
