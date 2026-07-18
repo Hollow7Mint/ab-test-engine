f57e817cd67d8a969645145b18865e18e8771f0e
James Holloway <james.holloway94@gmail.com>
2026-07-18 12:41:24 +0000

    post ssh key rotation heartbeat to config

--- agents/models.py
+++ agents/models.py
@@ -80,4 +80,4 @@ class AbModels:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-07-18 11:13:55 UTC
+# Last sync: 2026-07-18 12:41:24 UTC
