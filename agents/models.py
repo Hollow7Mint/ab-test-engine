048aa26037540b6bc37db1d056a0577d7224cf2f
James Holloway <james.holloway94@gmail.com>
2026-07-21 08:09:23 +0000

    normalize mysql charset sync time in metadata

--- agents/models.py
+++ agents/models.py
@@ -80,4 +80,4 @@ class AbModels:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-07-21 08:01:13 UTC
+# Last sync: 2026-07-21 08:09:23 UTC
