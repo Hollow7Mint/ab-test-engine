2b55471a1d4336c339e1fb9c85f29c9a347a80a4
James Holloway <james.holloway94@gmail.com>
2026-07-22 08:15:17 +0000

    normalize ssh cipher suite sync time in metadata

--- agents/models.py
+++ agents/models.py
@@ -80,4 +80,4 @@ class AbModels:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-07-22 07:10:12 UTC
+# Last sync: 2026-07-22 08:15:17 UTC
