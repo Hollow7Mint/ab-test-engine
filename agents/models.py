05a830887331420f3d9169301019394d871f09b3
James Holloway <james.holloway94@gmail.com>
2026-07-29 15:35:26 +0000

    log shield ddos event time in protection banner

--- agents/models.py
+++ agents/models.py
@@ -80,4 +80,4 @@ class AbModels:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-07-29 14:41:36 UTC
+# Last sync: 2026-07-29 15:35:26 UTC
