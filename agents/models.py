0d41b8494f1d5f6add00343fbce10771497a2346
James Holloway <james.holloway94@gmail.com>
2026-07-30 15:22:18 +0000

    anchor cloudwatch alarm threshold time in config

--- agents/models.py
+++ agents/models.py
@@ -80,4 +80,4 @@ class AbModels:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-07-30 14:24:18 UTC
+# Last sync: 2026-07-30 15:22:18 UTC
