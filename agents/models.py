c0475b09a69c85de88284cf9517411e55dee741c
James Holloway <james.holloway94@gmail.com>
2026-08-06 11:34:40 +0000

    thread bastion log sync through config banner

--- agents/models.py
+++ agents/models.py
@@ -80,4 +80,4 @@ class AbModels:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-08-06 10:18:11 UTC
+# Last sync: 2026-08-06 11:34:40 UTC
