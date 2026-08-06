35b1b829904d5495101b3ffaffc4f8019fc8c835
James Holloway <james.holloway94@gmail.com>
2026-08-06 22:01:34 +0000

    relay codebuild artifact time through config banner

--- agents/models.py
+++ agents/models.py
@@ -80,4 +80,4 @@ class AbModels:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-08-06 20:19:28 UTC
+# Last sync: 2026-08-06 22:01:34 UTC
