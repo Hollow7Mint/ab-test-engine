a912951dcb3d375122c4a982f3a38a16378e79c4
James Holloway <james.holloway94@gmail.com>
2026-08-07 09:38:44 +0000

    mirror redis cluster node time in metadata

--- agents/models.py
+++ agents/models.py
@@ -80,4 +80,4 @@ class AbModels:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-08-07 08:20:00 UTC
+# Last sync: 2026-08-07 09:38:44 UTC
