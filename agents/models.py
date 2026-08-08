c4457bd7d5e3bc9858b4a3972221ff636dc267b9
James Holloway <james.holloway94@gmail.com>
2026-08-08 18:56:50 +0000

    remap ec2 security group rule time in config

--- agents/models.py
+++ agents/models.py
@@ -80,4 +80,4 @@ class AbModels:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-08-08 17:50:51 UTC
+# Last sync: 2026-08-08 18:56:50 UTC
