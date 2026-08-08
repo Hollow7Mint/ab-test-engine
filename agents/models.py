a63b018f835d36431d8b1d136e0b9f13b2b2ab8c
James Holloway <james.holloway94@gmail.com>
2026-08-08 13:41:14 +0000

    align vpc subnet allocation time in config banner

--- agents/models.py
+++ agents/models.py
@@ -80,4 +80,4 @@ class AbModels:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-08-08 12:14:22 UTC
+# Last sync: 2026-08-08 13:41:14 UTC
