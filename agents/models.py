d5a6634dac2843214085de18c58907b66f3b3b5d
James Holloway <james.holloway94@gmail.com>
2026-08-09 12:22:00 +0000

    settle mysql innodb sync time after flush

--- agents/models.py
+++ agents/models.py
@@ -80,4 +80,4 @@ class AbModels:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-08-09 11:41:37 UTC
+# Last sync: 2026-08-09 12:22:00 UTC
