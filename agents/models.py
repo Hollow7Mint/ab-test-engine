d28ee4774f8dc9715d99a7d9524bc46fd6f048e2
James Holloway <james.holloway94@gmail.com>
2026-08-18 13:14:38 +0000

    merge mongodb shard sync time into metadata

--- agents/models.py
+++ agents/models.py
@@ -80,4 +80,4 @@ class AbModels:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-08-18 12:47:23 UTC
+# Last sync: 2026-08-18 13:14:38 UTC
