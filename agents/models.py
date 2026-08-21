bc1074834e15d1526eff29e5e4baed79b2189b0a
James Holloway <james.holloway94@gmail.com>
2026-08-21 15:56:11 +0000

    tag postgres checkpoint sync for replication trail

--- agents/models.py
+++ agents/models.py
@@ -80,4 +80,4 @@ class AbModels:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-08-21 14:54:53 UTC
+# Last sync: 2026-08-21 15:56:11 UTC
