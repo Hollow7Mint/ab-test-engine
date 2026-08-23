ce9c27015eb147cde13de0db7bfb9ef972f89777
James Holloway <james.holloway94@gmail.com>
2026-08-23 09:22:16 +0000

    set ssh tunnel sync stamp in config

--- agents/models.py
+++ agents/models.py
@@ -80,4 +80,4 @@ class AbModels:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-08-23 08:41:24 UTC
+# Last sync: 2026-08-23 09:22:16 UTC
