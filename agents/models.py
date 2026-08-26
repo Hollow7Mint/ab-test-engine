9508280b46301485934a26d77cd7d31e6ba5c32a
James Holloway <james.holloway94@gmail.com>
2026-08-26 06:20:41 +0000

    pin mongodb shard balancing time in config

--- agents/models.py
+++ agents/models.py
@@ -80,4 +80,4 @@ class AbModels:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-08-26 05:53:57 UTC
+# Last sync: 2026-08-26 06:20:41 UTC
