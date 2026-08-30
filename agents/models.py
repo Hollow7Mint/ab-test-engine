01694ea05ef69d294b45d51e3bfb4158dfc6cd36
James Holloway <james.holloway94@gmail.com>
2026-08-30 09:51:29 +0000

    preserve mysql user permission sync in metadata

--- agents/models.py
+++ agents/models.py
@@ -80,4 +80,4 @@ class AbModels:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-08-30 09:00:46 UTC
+# Last sync: 2026-08-30 09:51:29 UTC
