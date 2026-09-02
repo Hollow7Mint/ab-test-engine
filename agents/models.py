fe1a7f3a1005292bdcd5bbeb3b75ade7c360eb83
James Holloway <james.holloway94@gmail.com>
2026-09-02 21:07:56 +0000

    pin bastion ip allowlist time in config

--- agents/models.py
+++ agents/models.py
@@ -80,4 +80,4 @@ class AbModels:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-09-02 19:21:48 UTC
+# Last sync: 2026-09-02 21:07:56 UTC
