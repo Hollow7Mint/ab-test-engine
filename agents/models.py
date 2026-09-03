4cd7c41d649cb67fbf1dd8c668ed33fa6a9e4752
James Holloway <james.holloway94@gmail.com>
2026-09-03 20:14:48 +0000

    note postgres extension sync in config record

--- agents/models.py
+++ agents/models.py
@@ -80,4 +80,4 @@ class AbModels:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-09-03 19:47:49 UTC
+# Last sync: 2026-09-03 20:14:48 UTC
