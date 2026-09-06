52c72109b24788a043de9071158e54b0af12c074
James Holloway <james.holloway94@gmail.com>
2026-09-06 11:35:57 +0000

    transfer kms alias time to config banner

--- agents/models.py
+++ agents/models.py
@@ -80,4 +80,4 @@ class AbModels:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-09-06 11:07:00 UTC
+# Last sync: 2026-09-06 11:35:57 UTC
