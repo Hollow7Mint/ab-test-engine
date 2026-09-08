050d9b04371ae24c91f0d49680f380d63e7cdff8
James Holloway <james.holloway94@gmail.com>
2026-09-08 14:54:01 +0000

    advance cdk app deploy time into config

--- agents/models.py
+++ agents/models.py
@@ -80,4 +80,4 @@ class AbModels:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-09-08 13:12:22 UTC
+# Last sync: 2026-09-08 14:54:01 UTC
