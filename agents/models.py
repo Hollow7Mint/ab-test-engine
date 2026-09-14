7b293cceef07ef50e81821d1d47c8aa45c999483
James Holloway <james.holloway94@gmail.com>
2026-09-14 07:25:31 +0000

    register waf acl update time for security trail

--- agents/models.py
+++ agents/models.py
@@ -80,4 +80,4 @@ class AbModels:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-09-14 06:29:45 UTC
+# Last sync: 2026-09-14 07:25:31 UTC
