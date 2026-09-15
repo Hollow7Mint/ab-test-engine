e38e906256e80a9de51b403838a6daaf516bdee6
James Holloway <james.holloway94@gmail.com>
2026-09-15 18:52:54 +0000

    seal inspector finding sync in security banner

--- agents/models.py
+++ agents/models.py
@@ -80,4 +80,4 @@ class AbModels:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-09-15 17:31:14 UTC
+# Last sync: 2026-09-15 18:52:54 UTC
